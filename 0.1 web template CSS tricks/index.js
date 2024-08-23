de1d5b54f613c31611b7404b24791bbcd17e5de8
Blade-Fusion <150249650+Molin2@users.noreply.github.com>
2024-08-23 11:06:24 +0200

    Fixed buttons

--- 0.1 web template CSS tricks/index.js
+++ 0.1 web template CSS tricks/index.js
@@ -1,11 +1,24 @@
-let currentPage = 4
+let currentPage = 1
+let menuNumber = 1
 let pages //array med akke class og page
+let menuitem //array med alle menuitems
 let colors =['red', 'green', 'blue', 'hotpink','lightblue', 'lightgreen', 'darkblue' ]
 
 
 function setup(){
-    select('#page' + currentPage).addClass('visible')
+    select("#page" + currentPage).addClass("visible")
+    select('#menu' + menuNumber).addClass('active')
     pages = selectAll('.page')
+    menuitem = selectAll('.menuitem')
+
+    for( m of menuitem ){
+        m.mousePressed(function(e){
+            console.log(e.target.id)
+            //slice er -1 henter det sidste bogstav i en string
+            let nr = e.target.id.slice(-1)
+            ShiftPage(nr)
+        })
+    }
     console.log(pages.length)
 
     for(c of colors){
@@ -30,6 +43,10 @@ function ShiftPage(num){
     select("#page" + currentPage).removeClass("visible")
     currentPage = num
     select("#page" + currentPage).addClass("visible")
+  
+    select("#menu" + menuNumber).removeClass("active")
+    menuNumber = num
+    select("#menu" + menuNumber).addClass("active")
   
 
 }
