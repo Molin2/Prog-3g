d0299e88484d5c975209006a7e1aa58eed192d35
Blade-Fusion <150249650+Molin2@users.noreply.github.com>
2024-10-01 09:44:18 +0200

    data from reddit

--- Asynkron/Kapitel_15_opgave_single_page_template/index.js
+++ Asynkron/Kapitel_15_opgave_single_page_template/index.js
@@ -1,55 +1,41 @@
 function setup(){
-    hentTopPosts("cats");
+    hentTopPosts("Slipknot");
     // Kalder funktionen 'hentTopPosts' med argumentet "cats" ved programmets start.
 }
 
+//async betyder at funktionen kan vente på at ting er blevet hentet 
 async function hentTopPosts(subreddit) {
-    try {
-        const response = await fetch(`https://www.reddit.com/r/${subreddit}/top.json?limit=5`);
-        // Bruger 'fetch' til at hente data fra Reddit API'et for den angivne 'subreddit'.
-        // 'await' venter på, at 'fetch' anmodningen fuldføres.
-
-        const data = await response.json();
-        // Parser responsen til JSON-format.
-        // 'await' venter på, at parsing er færdig.
-
-        const posts = data.data.children;
-        // Ekstraherer arrayet af indlæg fra den modtagne data.
-
-        console.log(`Top 5 indlæg fra r/${subreddit}:`);
-        for (const post of posts) {
-            createPost(post.data);
-            // Itererer over hvert indlæg og kalder 'createPost' for at oprette det på siden.
-        }
-    } catch (fejl) {
-        console.error('Der opstod en fejl:', fejl);
-        // Håndterer eventuelle fejl, der opstår under hentningen af data.
+   const response = await fetch(`https://www.reddit.com/r/${subreddit}/top.json?limit=8`)
+    const json = await response.json()
+    console.log(json.data.children)
+    //posts er et array med posts fra json objektet
+    let posts = json.data.children
+    // vi kigger gennem arrayet og logger egenskaber i det 
+    for( p of posts ){
+        console.log(p.data.ups)
+        console.log(p.data.thumpnail)
+        console.log(p.data.name)
+        console.log(p.data.author)
+        console.log(p.data.url)
+        console.log(p.data.title)
+        createPost(p.data)
     }
+    
 }
 
 function createPost(post){
-    console.log(post);
-    // Logger hele indlægsobjektet til konsollen for debugging.
+    //laver først en refferance til html som vi vil sætte post in i 
+    let rightDiv = select('#page .right')
+    //giv posten en container
+    let container = createDiv().addClass('post')
+    // giv den en title
+    let title = createElement(h1, post.title)
+    container.
 
-    let htmlDiv = select('#page1 .right');
-    // Vælger HTML-elementet, hvor indlægget skal indsættes (den højre kolonne).
 
-    let container = createElement('div').addClass('post');
-    // Opretter et nyt 'div' element og tilføjer klassen 'post' for styling.
 
-    container.style('background-image', `url(${post.thumbnail})`);
-    // Sætter baggrundsbilledet til indlæggets thumbnail.
 
-    let title = createElement('h2', post.title);
-    // Opretter et 'h2' element med indlæggets titel.
 
-    let author = createElement('p', post.author).addClass('author');
-    // Opretter et 'p' element med forfatterens navn og tilføjer klassen 'author'.
+    rightDiv.child(container)
 
-    container.child(title);
-    container.child(author);
-    // Tilføjer 'title' og 'author' elementerne som børn til 'container'.
-
-    htmlDiv.child(container);
-    // Tilføjer hele 'container' til den valgte HTML-div, så det vises på siden.
-} 
+}
